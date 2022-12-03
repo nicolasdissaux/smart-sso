671beb7d7c9c0ccc626a2dd939fe59d1077256b2
zhoule <zhoule@daojia-inc.com>
2022-12-03 14:37:28 +0800

    使用@Mapper注解方式标示

--- smart-service/smart-system/src/main/java/com/smart/system/mapper/UserMapper.java
+++ smart-service/smart-system/src/main/java/com/smart/system/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.smart.system.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.smart.system.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
